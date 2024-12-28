23c55425309459c9ae4d5f1a15646d398961a034
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-12-28 17:13:18 +0600

    understand orbit controls

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 
 const ThreeJSExample = () => {
   const mountRef = useRef(null)
@@ -21,15 +23,22 @@ const ThreeJSExample = () => {
     );
 
     // 3. Create a Renderer
-    const renderer = new THREE.WebGLRenderer();
+    // antialias: true; will make element smooth and remove line
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
+
+    window.addEventListener('resize', () => {
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      camera.aspect = window.innerWidth / window.innerHeight
+      camera.updateProjectionMatrix();
+    })
 
     // Attach renderer to the DOM
     mountRef.current.appendChild(renderer.domElement);
 
     // Geometry and Material (A Cube)
     const geometry = new THREE.BoxGeometry(1, 1, 1); // Shape: Box
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Color: Green
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true }); // Color: Green
     const cube = new THREE.Mesh(geometry, material);  // Combine shape + color
 
     // set cube/mesh position
@@ -38,23 +47,38 @@ const ThreeJSExample = () => {
     // cube.position.y= 1
     // cube.position.z= 1
 
+    // cube.scale.z= 1
+    // cube.scale.x= 1
+    // cube.scale.y= 1
+
     scene.add(cube); // Add cube/mesh to the scene
 
     // distance of camera from object or Position the Camera
-    // camera.position.x = 1; // Move the camera to the right
-    // camera.position.y = -1; // Move the camera up
+    // camera.position.x = Math.PI / 4; // Move the camera to the right
+    // camera.position.y = Math.PI / 4 // Move the camera up
     camera.position.z = 2; // Move the camera further away
+
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    controls.enableDamping = true;
+
+    // Set the damping factor (adjust for desired smoothness)
+    controls.dampingFactor = 0.10;
+    controls.enableZoom = true;
     
     // Animation Loop
     // renderer.render(scene, camera);
     const animate = () => {
       requestAnimationFrame(animate);
-
+      // 3.14 means pie means 360deg
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
       cube.rotation.z += 0.01;
 
       // Render the Scene
+      // Render means take picture every time and print 
+      controls.update();
       renderer.render(scene, camera);
     };
     animate();
